aca661536823e98bd0f18af7c0a4dd9fa4c93a78
Diptanshu Datta <161836666+Diptanshu297@users.noreply.github.com>
2025-06-21 14:19:11 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,23 +1,20 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-
 
 load_dotenv()
 
-
 api_key = os.getenv("GROQ_API_KEY")
-
 
 llm = ChatGroq(
     groq_api_key=api_key,
-    model_name="llama-3-3-70b-versatile"
+    model_name="llama-3-70b-8192"
 )
 
-
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingredients in samosa are:")
+    response = llm.invoke("Two most important ingredients in samosa are")
     print(response.content)
+
 
 
 
